9062116668185236da69ffb4fba42666d511a713
Martin Eggen <meggen@redhat.com>
2024-06-07 15:39:03 +0200

    Initialize tables on startup
    Use Flask-Migrate to create database on startup

--- u13/__init__.py
+++ u13/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_migrate import Migrate
 from .extensions import db
 from .routes import short
 from .auth import auth
@@ -9,5 +10,10 @@ def create_app(config_file='settings.py', test_config=None):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
     db.init_app(app)
+    migrate = Migrate(app, db)
     app.register_blueprint(short)
+
+    with app.app_context():
+        db.create_all()
+
     return app
